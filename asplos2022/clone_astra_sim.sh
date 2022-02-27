21a20e7065eb6d1719b9a1fcddb50063bf4d87bd
Will Won <william.won@gatech.edu>
2022-02-27 03:04:57 -0500

    Add analytical backend branch setup

--- asplos2022/clone_astra_sim.sh
+++ asplos2022/clone_astra_sim.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+set -e
+
+# 1. Clone ASTRA-sim
 git clone --recursive https://github.com/astra-sim/astra-sim.git
+
+# 2. Set analytical backend branch
+(
+cd ./astra-sim/extern/network_backend/analytical/
+git checkout asplos2022-tutorial
+)
