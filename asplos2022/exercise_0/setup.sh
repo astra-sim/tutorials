bdaf70fd185535b14a40e5493cfffe85aedd67e5
Taekyung Heo <tkheo@casys.kaist.ac.kr>
2022-02-27 17:15:30 +0900

    Update setup scripts

--- asplos2022/exercise_0/setup.sh
+++ asplos2022/exercise_0/setup.sh
@@ -1,2 +1,9 @@
+cd astra-sim
+git checkout .
+git clean -xdf .
+cd ..
+
 cp -r exercise_0/examples/ astra-sim/
 cp -r exercise_0/inputs/ astra-sim/
+cd astra-sim
+./build/astra_analytical/build.sh -c
