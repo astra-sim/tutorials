64d49505a2c180dcf054bc8cdf729978bfd42503
Will Won <william.won@gatech.edu>
2022-06-18 00:07:53 -0400

    Update isca2022 tutorial

--- isca2022/clone_astra_sim.sh
+++ isca2022/clone_astra_sim.sh
@@ -5,10 +5,14 @@ set -e
 SCRIPT_DIR=$(dirname "$(realpath $0)")
 
 # 1. Clone ASTRA-sim
+(
 git clone --recursive https://github.com/astra-sim/astra-sim.git
+cd ${SCRIPT_DIR}/astra-sim/
+git checkout tutorial/isca2022
+)
 
 # 2. Set analytical backend branch
 (
 cd ${SCRIPT_DIR}/astra-sim/extern/network_backend/analytical/
-git checkout asplos2022-tutorial
+git checkout tutorial/isca2022
 )
