2098a01e107aa2dd31b01f27d66f86ac94b12f2b
Will Won <william.won@gatech.edu>
2022-09-21 04:57:01 -0400

    Update checkout branches

--- isca2022/clone_astra_sim.sh
+++ isca2022/clone_astra_sim.sh
@@ -8,11 +8,11 @@ SCRIPT_DIR=$(dirname "$(realpath $0)")
 (
 git clone --recursive https://github.com/astra-sim/astra-sim.git
 cd ${SCRIPT_DIR}/astra-sim/
-git checkout tutorial/isca2022
+git checkout tags/tutorial-isca2022
 )
 
 # 2. Set analytical backend branch
 (
 cd ${SCRIPT_DIR}/astra-sim/extern/network_backend/analytical/
-git checkout tutorial/isca2022
+git checkout tags/tutorial-isca2022
 )
