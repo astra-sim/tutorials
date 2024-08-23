18d797864091e447ae5dcfa8ec6e3d11beb4511b
Will Won <william.won@gatech.edu>
2024-08-23 14:53:26 -0400

    Implement demo2

--- hoti2024/install_chakra.sh
+++ hoti2024/install_chakra.sh
@@ -6,7 +6,8 @@ SCRIPT_DIR=$(dirname "$(realpath $0)")
 
 # Install Chakra
 (
-cd ${SCRIPT_DIR}/astra-sim/extern/graph_frontend/chakra
+ln -s ${SCRIPT_DIR}/astra-sim/extern/graph_frontend/chakra ${SCRIPT_DIR}/chakra . # create symlink for easy access
+cd ${SCRIPT_DIR}/chakra
 pip3 install .
 pip3 install --upgrade protobuf
 )
