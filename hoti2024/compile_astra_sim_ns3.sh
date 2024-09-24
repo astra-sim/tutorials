8e722b97d1fa0b898f232ae45cedde74bf44268a
Jinsun Yoo <jinsun.official@gmail.com>
2024-09-24 07:04:23 +0000

    Dockerfile dependencies for ns3

---
+++ hoti2024/compile_astra_sim_ns3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Path
+SCRIPT_DIR=$(dirname "$(realpath $0)")
+
+# Compile ASTRA-sim with ns3 backend model
+(
+cd ${SCRIPT_DIR}/astra-sim
+bash ./build/astra_ns3/build.sh -c
+)
