0232aa78b6308026c66847a30cc78beb49fffd99
Jinsun Yoo <jinsun.official@gmail.com>
2024-09-30 05:05:14 +0000

    Add symlink to wokrload allreduce

--- hoti2024/compile_astra_sim_ns3.sh
+++ hoti2024/compile_astra_sim_ns3.sh
@@ -17,3 +17,5 @@ ln -s astra-sim/extern/network_backend/ns-3/scratch/config/config.txt ./demo4/in
 ln -s astra-sim/extern/network_backend/ns-3/scratch/config/config_clos.txt ./demo4/inputs/ns3_config_clos.txt
 ln -s astra-sim/extern/network_backend/ns-3/scratch/topology/8_nodes_1_switch_topology.txt ./demo4/inputs/8_nodes_1_switch.txt
 ln -s astra-sim/extern/network_backend/ns-3/scratch/topology/128_nodes_16_switch_topology.txt ./demo4/inputs/128_nodes_16_switch_clos.txt
+ln -s $(realpath ./demo1/allreduce) ./demo4/allreduce
+
