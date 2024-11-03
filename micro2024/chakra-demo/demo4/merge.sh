0778fcae6c8e39ba5e75180eaa89f12780bc68cd
JoongunPark <8554137+JoongunPark@users.noreply.github.com>
2024-11-03 13:06:10 -0500

    Update traces

--- micro2024/chakra-demo/demo4/merge.sh
+++ micro2024/chakra-demo/demo4/merge.sh
@@ -11,14 +11,14 @@ mkdir -p ${SCRIPT_DIR}/json_traces
 # Merge Traces: NPU 0
 chakra_trace_link \
     --rank=0 \
-    --chakra-host-trace=${SCRIPT_DIR}/pytorch_traces/pytorch_et_0.json \
-    --chakra-device-trace=${SCRIPT_DIR}/pytorch_traces/kineto_0.json \
+    --chakra-host-trace=${SCRIPT_DIR}/pytorch_traces/host_0.json \
+    --chakra-device-trace=${SCRIPT_DIR}/pytorch_traces/device_0.json \
     --output-file=${SCRIPT_DIR}/json_traces/hdt_0.json
 
 # Merge Traces: NPU 1
 chakra_trace_link \
     --rank=1 \
-    --chakra-host-trace=${SCRIPT_DIR}/pytorch_traces/pytorch_et_1.json \
-    --chakra-device-trace=${SCRIPT_DIR}/pytorch_traces/kineto_1.json \
+    --chakra-host-trace=${SCRIPT_DIR}/pytorch_traces/host_1.json \
+    --chakra-device-trace=${SCRIPT_DIR}/pytorch_traces/device_1.json \
     --output-file=${SCRIPT_DIR}/json_traces/hdt_1.json
 )
