fbbdd8e4d8c0839b442e354e966d78e3b0d3a3aa
Jinsun Yoo <jinsun.official@gmail.com>
2024-11-03 09:29:01 -0500

    Files for ns-3 tutorial

---
+++ micro2024/astra-sim-demo/demo3/run_demo3-2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Path
+SCRIPT_DIR=$(dirname "$(realpath $0)")
+ASTRA_SIM_BUILD_DIR=${SCRIPT_DIR}/../../astra-sim/extern/network_backend/ns-3/build/scratch/
+ASTRA_SIM=./ns3.42-AstraSimNetwork-default
+
+# Run ASTRA-sim
+(
+cd ${ASTRA_SIM_BUILD_DIR}
+touch ../../scratch/output/flow.txt
+${ASTRA_SIM} \
+    --workload-configuration=${SCRIPT_DIR}/allreduce_2D/allreduce \
+    --system-configuration=${SCRIPT_DIR}/inputs/Ring_Ring_sys.json \
+    --remote-memory-configuration=${SCRIPT_DIR}/inputs/RemoteMemory.json \
+    --logical-topology-configuration=${SCRIPT_DIR}/inputs/logical_128nodes_2D.json \
+    --network-configuration=../../../ns-3/scratch/config/config_clos.txt \
+)
